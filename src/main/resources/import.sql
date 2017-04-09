558c20c79a150d6431b8779ec25933cbb012f131
Denis Olek <denis.olek@gmail.com>
2017-04-09 11:56:03 +0200

    TAB-7 Added import sql file for mock db data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into "user" (email, name, surname) values ('john.diggle@gmail.com', 'John', 'Diggle');
+insert into "user" (email, name, surname) values ('emily.wong@gmail.com', 'Emily', 'Wong');
+insert into "user" (email, name, surname) values ('judah.sanchez@gmail.com', 'Judah', 'Sanchez');
