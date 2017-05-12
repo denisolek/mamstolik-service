dc5a32fe359bdbfae72e4e22f2f33805e5bd9436
denisolek <denis.olek@gmail.com>
2017-05-13 00:02:19 +0200

    TAB-20 Changed reservationEnd to LocalDateTime

--- src/main/java/pl/denisolek/Reservation/Reservation.java
+++ src/main/java/pl/denisolek/Reservation/Reservation.java
@@ -3,15 +3,12 @@ package pl.denisolek.Reservation;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import pl.denisolek.BaseEntity;
-import pl.denisolek.Converters.LocalDateTimeAttributeConverter;
 import pl.denisolek.Customer.Customer;
 import pl.denisolek.Restaurant.Restaurant;
 
-import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Data
@@ -27,10 +24,9 @@ public class Reservation extends BaseEntity {
 	@JoinColumn
 	Customer customer;
 
-	@Convert(converter = LocalDateTimeAttributeConverter.class)
 	LocalDateTime reservationBegin;
 
-	Timestamp reservationEnd;
+	LocalDateTime reservationEnd;
 
 	Integer peopleNumber;
 
