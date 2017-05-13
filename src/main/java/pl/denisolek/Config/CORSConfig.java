a7f1285b381fb109985b58e8cb51a64d49cba783
denisolek <denis.olek@gmail.com>
2017-05-13 18:40:19 +0200

    TAB-35 CORS config

---
+++ src/main/java/pl/denisolek/Config/CORSConfig.java
@@ -0,0 +1,28 @@
+package pl.denisolek.Config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSConfig extends CorsFilter {
+
+    public CORSConfig() {
+        super(configurationSource());
+    }
+
+    private static UrlBasedCorsConfigurationSource configurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
