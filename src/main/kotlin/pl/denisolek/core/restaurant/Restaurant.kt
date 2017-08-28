817fcfd8477acc46e7a2fc0ac43c90ebf2b03e66
denisolek <denis.olek@gmail.com>
2017-08-29 00:49:56 +0200

    TAB-135 added restaurant facilities

--- src/main/kotlin/pl/denisolek/core/restaurant/Restaurant.kt
+++ src/main/kotlin/pl/denisolek/core/restaurant/Restaurant.kt
@@ -29,6 +29,12 @@ class Restaurant(
         @Column(name = "kitchen_type", nullable = false)
         var kitchenTypes: MutableSet<KitchenType>,
 
+        @ElementCollection(fetch = FetchType.EAGER)
+        @Enumerated(EnumType.STRING)
+        @CollectionTable(name = "restaurant_facility", joinColumns = arrayOf(JoinColumn(name = "restaurantId")))
+        @Column(name = "facility", nullable = false)
+        var facilities: MutableSet<Facilities>,
+
         @OneToMany(cascade = arrayOf(CascadeType.ALL))
         @JoinTable(name = "restaurant_business_hour", joinColumns = arrayOf(JoinColumn(name = "restaurant_id")), inverseJoinColumns = arrayOf(JoinColumn(name = "business_hour_id")))
         var businessHours: MutableSet<BusinessHour> = mutableSetOf()
@@ -83,5 +89,41 @@ class Restaurant(
         GLUTEN_FREE,
         BAR,
         CAFE
+    }
+
+    enum class Facilities {
+        AIR_CONDITIONING,
+        SMOKING_ROOM,
+        NON_SMOKING_ROOM,
+        TERRACE,
+        GARDEN,
+        PARKING,
+        FREE_WIFI,
+        TOILET_FOR_DISABLED,
+        DISABLED_PEOPLE_IMPROVEMENTS,
+        CHILDRENS_MENU,
+        CHILD_SEATS,
+        PLAYGROUND,
+        MONITORED_PARKING,
+        BABY_CORNER,
+        PETS_FRIENDLY,
+        BILLIARDS,
+        DARTS,
+        TV,
+        BICYCLES_STANDS,
+        VIP_ROOM,
+        SEPARATE_BANQUET_HALL,
+        CLOSE_TO_HOTEL,
+        ALCOHOL,
+        TAKEAWAY,
+        PAYMENT_AMERICAN_EXPRESS,
+        PAYMENT_MASTERCARD,
+        PAYMENT_VISA,
+        BOWLING,
+        PLASTIC_PLATES,
+        BOILING_WATER,
+        BABY_TOILET,
+        PRESS,
+        SPORTS_BROADCAST
     }
 }
