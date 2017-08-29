89257577f2ef9cb4e74331a76298ec6f933cdbab
denisolek <denis.olek@gmail.com>
2017-08-29 22:50:17 +0200

    TAB-139 created RestaurantDTO for guests with mapping list of restaurants function

---
+++ src/main/kotlin/pl/denisolek/guest/restaurant/DTO/RestaurantDTO.kt
@@ -0,0 +1,18 @@
+package pl.denisolek.guest.restaurant.DTO
+
+import pl.denisolek.core.restaurant.Restaurant
+
+data class RestaurantDTO(
+        val id: Int?,
+        val name: String) {
+
+    companion object {
+        fun fromRestaurantList(restaurants: List<Restaurant>): List<RestaurantDTO> =
+                restaurants.map {
+                    RestaurantDTO(
+                            id = it.id,
+                            name = it.name
+                    )
+                }
+    }
+}
