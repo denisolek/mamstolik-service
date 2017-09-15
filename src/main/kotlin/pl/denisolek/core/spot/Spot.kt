a5360efb820accaa20272fa6464b02b418b49e57
Denis Olek <denis.olek@gmail.com>
2017-09-15 02:32:28 +0200

    TAB-139 added min people number to spots

--- src/main/kotlin/pl/denisolek/core/spot/Spot.kt
+++ src/main/kotlin/pl/denisolek/core/spot/Spot.kt
@@ -2,12 +2,16 @@ package pl.denisolek.core.spot
 
 import pl.denisolek.core.BaseEntity
 import pl.denisolek.core.restaurant.Restaurant
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.ManyToOne
 
 @Entity
 data class Spot(
         var capacity: Int,
+
+        @Column(nullable = false, columnDefinition = "int default 1")
+        var minPeopleNumber: Int = 1,
 
         @ManyToOne
         var restaurant: Restaurant) : BaseEntity()
