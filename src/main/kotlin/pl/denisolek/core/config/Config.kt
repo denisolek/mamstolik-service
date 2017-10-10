e4ba71e99d461372517f5e0fe0aaa584a5a58118
denisolek <denis.olek@gmail.com>
2017-10-11 00:47:48 +0200

    TAB-330 created endpoint config

--- src/main/kotlin/pl/denisolek/core/config/Config.kt
+++
@@ -1,5 +0,0 @@
-package pl.denisolek.core.config
-
-const val API_BASE_PATH: String = "/api"
-const val PANEL_BASE_PATH: String = "/panel"
-const val ADMIN_BASE_PATH: String = "/admin"
--- src/main/kotlin/pl/denisolek/guest/restaurant/GuestRestaurantApi.kt
+++ src/main/kotlin/pl/denisolek/guest/restaurant/GuestRestaurantApi.kt
@@ -4,21 +4,23 @@ import io.swagger.annotations.Api
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import pl.denisolek.core.address.City
-import pl.denisolek.core.config.API_BASE_PATH
 import pl.denisolek.core.restaurant.Restaurant
 import pl.denisolek.guest.restaurant.DTO.RestaurantDetailsDTO
 import pl.denisolek.guest.restaurant.DTO.SearchDTO
+import pl.denisolek.infrastructure.API_BASE_PATH
 import springfox.documentation.annotations.ApiIgnore
 import java.time.LocalDateTime
 
 @Api(value = "GuestRestaurantApi")
+@RequestMapping(API_BASE_PATH)
 interface GuestRestaurantApi {
     companion object {
         const val RESTAURANT_ID: String = "restaurantId"
 
-        const val RESTAURANTS_BASE_PATH = "$API_BASE_PATH/restaurants"
+        const val RESTAURANTS_BASE_PATH = "/restaurants"
         const val GET_RESTAURANT_PATH = "$RESTAURANTS_BASE_PATH/{$RESTAURANT_ID}"
     }
 
