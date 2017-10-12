cb62a2f897e977cb2d4ed0ed0087fc44ac6a6a2b
denisolek <denis.olek@gmail.com>
2017-10-12 23:37:33 +0200

    TAB-330 UserService with username generator

---
+++ src/main/kotlin/pl/denisolek/core/user/UserService.kt
@@ -0,0 +1,22 @@
+package pl.denisolek.core.user
+
+import org.springframework.stereotype.Service
+import pl.denisolek.infrastructure.util.generateUsernameString
+
+@Service
+class UserService(val userRepository: UserRepository) {
+    fun save(user: User) =
+            userRepository.save(user)
+
+    fun generateUsername(): String {
+        var exists: Boolean = true
+        var username: String = "username"
+
+        while (exists) {
+            username = generateUsernameString()
+            if (userRepository.countByUsername(username) == 0)
+                exists = false
+        }
+        return username
+    }
+}
