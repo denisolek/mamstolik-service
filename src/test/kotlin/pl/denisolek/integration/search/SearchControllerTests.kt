7175c8124fb37b0e4954302c9037e381224c578f
denisolek <denis.olek@gmail.com>
2017-10-13 00:50:36 +0200

    TAB-330 fixed integration tests

--- src/test/kotlin/pl/denisolek/integration/search/SearchControllerTests.kt
+++ src/test/kotlin/pl/denisolek/integration/search/SearchControllerTests.kt
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.context.WebApplicationContext
 import pl.denisolek.core.address.City
+import pl.denisolek.infrastructure.API_BASE_PATH
 import pl.denisolek.infrastructure.util.convertJsonBytesToObject
 import pl.denisolek.shared.search.SearchController
 import pl.denisolek.shared.search.dto.CitiesRestaurantsDTO
@@ -31,6 +32,8 @@ class SearchControllerTests {
 
     lateinit var mvc: MockMvc
 
+    val SEARCH_PATH = "$API_BASE_PATH/${SearchController.API.SEARCH_PATH}"
+
     @Before
     fun setup() {
         this.mvc = MockMvcBuilders.webAppContextSetup(applicationContext).build()
@@ -38,9 +41,7 @@ class SearchControllerTests {
 
     @Test
     fun `findCitiesAndRestaurants_ name = p`() {
-        val url = SearchController.API.SEARCH_PATH
-
-        val result = mvc.perform(get(url).param("name", "p"))
+        val result = mvc.perform(get(SEARCH_PATH).param("name", "p"))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
 
@@ -62,9 +63,7 @@ class SearchControllerTests {
 
     @Test
     fun `findCitiesAndRestaurants_ name = po`() {
-        val url = SearchController.API.SEARCH_PATH
-
-        val result = mvc.perform(get(url).param("name", "po"))
+        val result = mvc.perform(get(SEARCH_PATH).param("name", "po"))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
 
@@ -83,9 +82,7 @@ class SearchControllerTests {
 
     @Test
     fun `findCitiesAndRestaurants_ name = gorzow`() {
-        val url = SearchController.API.SEARCH_PATH
-
-        val result = mvc.perform(get(url).param("name", "gorzow"))
+        val result = mvc.perform(get(SEARCH_PATH).param("name", "gorzow"))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
 
@@ -103,9 +100,7 @@ class SearchControllerTests {
 
     @Test
     fun `findCitiesAndRestaurants_ name = gw`() {
-        val url = SearchController.API.SEARCH_PATH
-
-        val result = mvc.perform(get(url).param("name", "gw"))
+        val result = mvc.perform(get(SEARCH_PATH).param("name", "gw"))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
 
@@ -122,9 +117,7 @@ class SearchControllerTests {
 
     @Test
     fun `findCitiesAndRestaurants_ name = pzn`() {
-        val url = SearchController.API.SEARCH_PATH
-
-        val result = mvc.perform(get(url).param("name", "pzn"))
+        val result = mvc.perform(get(SEARCH_PATH).param("name", "pzn"))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
 
@@ -141,9 +134,7 @@ class SearchControllerTests {
 
     @Test
     fun `findCitiesAndRestaurants_ name = man`() {
-        val url = SearchController.API.SEARCH_PATH
-
-        val result = mvc.perform(get(url).param("name", "man"))
+        val result = mvc.perform(get(SEARCH_PATH).param("name", "man"))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
 
