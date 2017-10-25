d611fd671e0697a7664b6da9bfcbe5a82d9d3117
denisolek <denis.olek@gmail.com>
2017-10-25 22:56:52 +0200

    TAB-330 fixed security at /panel endpoints

--- src/main/java/pl/denisolek/Config/ResourceServerConfig.java
+++ src/main/java/pl/denisolek/Config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package pl.denisolek.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -16,10 +17,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         http.headers().frameOptions().disable().and()
                 .authorizeRequests()
                 .antMatchers("/api").permitAll()
-//                .antMatchers(HttpMethod.PUT, "/panel/users/password").hasAuthority("ROLE_OWNER")
-//                .antMatchers("/panel/restaurants").hasAuthority("ROLE_OWNER")
-                .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                .antMatchers(HttpMethod.PUT, "/panel/users/password").hasAuthority("ROLE_OWNER")
+                .antMatchers("/panel/users", "/panel/users/**").permitAll()
                 .antMatchers("/panel/**").hasAuthority("ROLE_OWNER")
-                .antMatchers("/panel/users**").permitAll();
+                .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN");
     }
 }
