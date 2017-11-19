0a159c3eae0c154a6932f94840803fa1c0e220d0
denisolek <denis.olek@gmail.com>
2017-11-19 04:26:00 +0100

    TAB-361 Update schema existing items tests

--- src/test/kotlin/pl/denisolek/integration/panel/PanelSchemaControllerTests.kt
+++ src/test/kotlin/pl/denisolek/integration/panel/PanelSchemaControllerTests.kt
@@ -3,6 +3,7 @@ package pl.denisolek.integration.panel
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.whenever
 import org.hamcrest.Matchers
+import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -23,6 +24,7 @@ import org.springframework.web.context.WebApplicationContext
 import pl.denisolek.Exception.ServiceException
 import pl.denisolek.core.restaurant.RestaurantRepository
 import pl.denisolek.core.schema.Floor
+import pl.denisolek.core.schema.SchemaItem
 import pl.denisolek.core.user.UserRepository
 import pl.denisolek.infrastructure.PANEL_BASE_PATH
 import pl.denisolek.infrastructure.config.security.AuthorizationService
@@ -30,6 +32,9 @@ import pl.denisolek.infrastructure.util.convertJsonBytesToObject
 import pl.denisolek.infrastructure.util.convertObjectToJsonBytes
 import pl.denisolek.panel.schema.DTO.FloorDTO
 import pl.denisolek.panel.schema.DTO.SchemaDTO
+import pl.denisolek.panel.schema.DTO.type.SchemaDetailsDTO
+import pl.denisolek.panel.schema.DTO.type.SchemaPositionDTO
+import pl.denisolek.panel.schema.DTO.type.SchemaSpotInfoDTO
 import pl.denisolek.panel.schema.PanelSchemaApi
 import javax.transaction.Transactional
 
@@ -52,7 +57,7 @@ class PanelSchemaControllerTests {
 
     lateinit var mvc: MockMvc
 
-    val SCHEMA_PATH = "${PANEL_BASE_PATH}${PanelSchemaApi.SCHEMAS_PATH}"
+    val SCHEMAS_PATH = "${PANEL_BASE_PATH}${PanelSchemaApi.SCHEMAS_PATH}"
     val FLOORS_PATH = "${PANEL_BASE_PATH}${PanelSchemaApi.FLOORS_PATH}"
     val FLOORS_ID_PATH = "${PANEL_BASE_PATH}${PanelSchemaApi.FLOORS_ID_PATH}"
 
@@ -68,7 +73,7 @@ class PanelSchemaControllerTests {
     fun `getSchema_`() {
         val user = userRepository.findOne(1)
         doReturn(user).whenever(authorizationService).getCurrentUser()
-        val result = mvc.perform(get(SCHEMA_PATH, 1))
+        val result = mvc.perform(get(SCHEMAS_PATH, 1))
                 .andExpect(status().isOk)
                 .andReturn()
 
@@ -127,4 +132,124 @@ class PanelSchemaControllerTests {
             assertTrue(it.id != 2)
         }
     }
+
+    @Test
+    fun `updateSchema_ update existing item - floorId`() {
+        val schemaDTO = prepareUpdateSchemaDTO()
+        schemaDTO.tables[0].floorId = 2
+
+        val body = convertObjectToJsonBytes(schemaDTO)
+
+        val result = mvc.perform(put(SCHEMAS_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk)
+                .andReturn()
+
+        val actual = convertJsonBytesToObject(result.response.contentAsString, SchemaDTO::class.java)
+        Assert.assertEquals(2, actual.tables[0].floorId)
+    }
+
+    @Test
+    fun `updateSchema_ update existing item - floorId not existing`() {
+        val schemaDTO = prepareUpdateSchemaDTO()
+        schemaDTO.tables[0].floorId = 100
+
+        val body = convertObjectToJsonBytes(schemaDTO)
+
+        mvc.perform(put(SCHEMAS_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `updateSchema_ update existing item - subType`() {
+        val schemaDTO = prepareUpdateSchemaDTO()
+        schemaDTO.tables[0].subType = SchemaItem.TableType.EIGHT_ROUND
+
+        val body = convertObjectToJsonBytes(schemaDTO)
+
+        val result = mvc.perform(put(SCHEMAS_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk)
+                .andReturn()
+
+        val actual = convertJsonBytesToObject(result.response.contentAsString, SchemaDTO::class.java)
+        Assert.assertEquals(SchemaItem.TableType.EIGHT_ROUND, actual.tables[0].subType)
+    }
+
+    @Test
+    fun `updateSchema_ update existing item - position`() {
+        val schemaDTO = prepareUpdateSchemaDTO()
+        schemaDTO.tables[0].position = SchemaPositionDTO(10000f, 10000f)
+
+        val body = convertObjectToJsonBytes(schemaDTO)
+
+        val result = mvc.perform(put(SCHEMAS_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk)
+                .andReturn()
+
+        val actual = convertJsonBytesToObject(result.response.contentAsString, SchemaDTO::class.java)
+        Assert.assertEquals(SchemaPositionDTO(10000f, 10000f), actual.tables[0].position)
+    }
+
+    @Test
+    fun `updateSchema_ update existing item - details`() {
+        val schemaDTO = prepareUpdateSchemaDTO()
+        schemaDTO.tables[0].details = SchemaDetailsDTO(10000, 10000, 10000f)
+
+        val body = convertObjectToJsonBytes(schemaDTO)
+
+        val result = mvc.perform(put(SCHEMAS_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk)
+                .andReturn()
+
+        val actual = convertJsonBytesToObject(result.response.contentAsString, SchemaDTO::class.java)
+        Assert.assertEquals(SchemaDetailsDTO(10000, 10000, 10000f), actual.tables[0].details)
+    }
+
+    @Test
+    fun `updateSchema_ update existing table - spotInfo`() {
+        val schemaDTO = prepareUpdateSchemaDTO()
+        schemaDTO.tables[0].spotInfo = SchemaSpotInfoDTO(1, 100, 4, 1)
+
+        val body = convertObjectToJsonBytes(schemaDTO)
+
+        val result = mvc.perform(put(SCHEMAS_PATH, 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk)
+                .andReturn()
+
+        val actual = convertJsonBytesToObject(result.response.contentAsString, SchemaDTO::class.java)
+        Assert.assertEquals(SchemaSpotInfoDTO(1, 1, 4, 1), actual.tables[0].spotInfo)
+    }
+
+    private fun prepareUpdateSchemaDTO(): SchemaDTO {
+        val user = userRepository.findOne(1)
+        doReturn(user).whenever(authorizationService).getCurrentUser()
+        val schemas = mvc.perform(get(SCHEMAS_PATH, 1))
+                .andExpect(status().isOk)
+                .andReturn()
+
+        val schemaDTO = convertJsonBytesToObject(schemas.response.contentAsString, SchemaDTO::class.java)
+        schemaDTO.floors = null
+        return schemaDTO
+    }
+
+//    @Test
+//    fun `updateSchema_ add new item`() {
+//
+//    }
+//
+//    @Test
+//    fun `updateSchema_ remove item from schema (should not remove)`() {
+//
+//    }
 }
