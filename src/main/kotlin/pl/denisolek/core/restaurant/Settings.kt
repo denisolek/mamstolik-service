7ea41049cbc046d6b2e71296e082cc7429c0539b
denisolek <denis.olek@gmail.com>
2017-11-25 19:00:40 +0100

    TAB-394 RestaurantDetailsDTO is using restaurant settings now

--- src/main/kotlin/pl/denisolek/core/restaurant/Settings.kt
+++ src/main/kotlin/pl/denisolek/core/restaurant/Settings.kt
@@ -7,10 +7,10 @@ class Settings(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Int? = null,
-        var localization: Boolean? = false,
-        var special_dates: Boolean? = false,
-        var description: Boolean? = false,
-        var photos: Boolean? = false,
-        var menu: Boolean? = false,
-        var schema: Boolean? = false
+        var localization: Boolean = false,
+        var special_dates: Boolean = false,
+        var description: Boolean = false,
+        var photos: Boolean = false,
+        var menu: Boolean = false,
+        var schema: Boolean = false
 )
--- src/main/kotlin/pl/denisolek/guest/restaurant/DTO/RestaurantDetailsDTO.kt
+++ src/main/kotlin/pl/denisolek/guest/restaurant/DTO/RestaurantDetailsDTO.kt
@@ -8,10 +8,10 @@ import java.time.DayOfWeek
 data class RestaurantDetailsDTO(
         var id: Int? = null,
         var name: String,
-        var description: String,
+        var description: String?,
         var rate: RestaurantRateDTO,
         var address: Address,
-        var menu: List<MenuCategoryDTO>,
+        var menu: List<MenuCategoryDTO>?,
         var tags: List<Any>,
         var businessHours: MutableMap<DayOfWeek, BusinessHour>
 ) {
@@ -20,17 +20,31 @@ data class RestaurantDetailsDTO(
                 RestaurantDetailsDTO(
                         id = restaurant.id,
                         name = restaurant.name,
-                        description = restaurant.description,
-                        rate = RestaurantRateDTO(
-                                total = restaurant.rate,
-                                food_rate = restaurant.food_rate,
-                                price_quality_rate = restaurant.price_quality_rate,
-                                service_rate = restaurant.service_rate
-                        ),
+                        description = setDescription(restaurant),
+                        rate = setRate(restaurant),
                         address = restaurant.address!!,
-                        menu = restaurant.menu?.categories?.map { MenuCategoryDTO.fromMenuCategory(it) }?.sortedBy { it.position }!!,
+                        menu = setMenu(restaurant),
                         businessHours = restaurant.businessHours,
                         tags = listOf(restaurant.cuisineTypes, restaurant.facilities)
                 )
+
+        private fun setDescription(restaurant: Restaurant) =
+                if (restaurant.settings!!.description) restaurant.description else null
+
+        private fun setRate(restaurant: Restaurant): RestaurantRateDTO {
+            return RestaurantRateDTO(
+                    total = restaurant.rate,
+                    food_rate = restaurant.food_rate,
+                    price_quality_rate = restaurant.price_quality_rate,
+                    service_rate = restaurant.service_rate
+            )
+        }
+
+        private fun setMenu(restaurant: Restaurant): List<MenuCategoryDTO>? {
+            return if (restaurant.settings!!.menu)
+                restaurant.menu?.categories?.map { MenuCategoryDTO.fromMenuCategory(it) }?.sortedBy { it.position }!!
+            else
+                null
+        }
     }
 }
