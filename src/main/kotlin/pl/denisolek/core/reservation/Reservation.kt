1bd7c018631c38c6fd5ad9d9e53826c424c671e5
denisolek <denis.olek@gmail.com>
2017-11-26 01:56:20 +0100

    TAB-395 reservation from PanelReservationDTO

--- src/main/kotlin/pl/denisolek/core/reservation/Reservation.kt
+++ src/main/kotlin/pl/denisolek/core/reservation/Reservation.kt
@@ -1,23 +1,28 @@
 package pl.denisolek.core.reservation
 
-import pl.denisolek.core.BaseEntity
 import pl.denisolek.core.customer.Customer
 import pl.denisolek.core.restaurant.Restaurant
 import pl.denisolek.core.spot.Spot
+import pl.denisolek.core.user.User
 import pl.denisolek.infrastructure.util.DateTimeInterval
+import pl.denisolek.panel.reservation.DTO.PanelReservationDTO
 import java.time.Duration
 import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
 data class Reservation(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Int? = null,
         override var startDateTime: LocalDateTime,
         override var endDateTime: LocalDateTime,
         var peopleNumber: Int,
         var state: ReservationState,
-        var verificationCode: Int,
+        var verificationCode: Int? = null,
         var duration: Duration,
         var isVerified: Boolean? = false,
+        var note: String? = "",
 
         @ManyToOne
         var restaurant: Restaurant,
@@ -26,10 +31,29 @@ data class Reservation(
         @JoinColumn
         var customer: Customer,
 
+        @ManyToOne
+        @JoinColumn
+        var approvedBy: User? = null,
+
         @ManyToMany(fetch = FetchType.EAGER)
         @JoinTable(name = "reservation_spots", joinColumns = arrayOf(JoinColumn(name = "reservation_id")), inverseJoinColumns = arrayOf(JoinColumn(name = "spot_id")))
-        var spots: List<Spot>
-) : BaseEntity(), DateTimeInterval {
+        var spots: MutableList<Spot> = mutableListOf()
+) : DateTimeInterval {
+    constructor(panelReservationDTO: PanelReservationDTO, restaurant: Restaurant, customer: Customer, user: User, spots: MutableList<Spot>) : this(
+            startDateTime = panelReservationDTO.date,
+            endDateTime = panelReservationDTO.date.plus(restaurant.avgReservationTime),
+            peopleNumber = panelReservationDTO.peopleNumber,
+            state = ReservationState.ACCEPTED,
+            verificationCode = null,
+            duration = restaurant.avgReservationTime,
+            isVerified = true,
+            note = panelReservationDTO.note,
+            restaurant = restaurant,
+            customer = customer,
+            approvedBy = user,
+            spots = spots
+    )
+
     enum class ReservationState {
         PENDING,
         ACCEPTED,
