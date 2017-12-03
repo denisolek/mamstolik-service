fbf17591d41361bd694f7337f953b720903a3aee
Denis Olek <denis.olek@gmail.com>
2017-12-03 05:53:05 +0100

    TAB-411 assign main restaurant && refactor

--- src/main/kotlin/pl/denisolek/panel/image/PanelImageService.kt
+++ src/main/kotlin/pl/denisolek/panel/image/PanelImageService.kt
@@ -8,6 +8,7 @@ import pl.denisolek.core.image.Image
 import pl.denisolek.core.image.ImageService
 import pl.denisolek.core.image.StorageProperties
 import pl.denisolek.core.restaurant.Restaurant
+import pl.denisolek.core.restaurant.RestaurantService
 import pl.denisolek.infrastructure.FULL_SIZE
 import pl.denisolek.infrastructure.THUMBNAIL
 import pl.denisolek.infrastructure.TMP
@@ -19,27 +20,51 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 @Service
-class PanelImageService(private val imageService: ImageService, storageProperties: StorageProperties) {
+class PanelImageService(private val imageService: ImageService,
+                        storageProperties: StorageProperties,
+                        private val restaurantService: RestaurantService) {
 
     private val thumbnailLocation: Path = Paths.get(storageProperties.thumbnailLocation)
     private val fullSizeLocation: Path = Paths.get(storageProperties.fullSizeLocation)
 
     fun uploadImage(restaurant: Restaurant, imageType: String, image: MultipartFile): ImageDTO {
-        if (image.isEmpty) throw ServiceException(HttpStatus.BAD_REQUEST, "Image is empty.")
-        if (!image.isImageType()) throw ServiceException(HttpStatus.BAD_REQUEST, "Wrong image type.")
-
+        validateImage(image)
         val uuidName = imageService.generateUUID()
+        saveFullSizeThumbnail(uuidName, image)
+        val newImage = saveImage(image, uuidName, restaurant)
+        assignMainImage(imageType, restaurant, newImage)
+        return ImageDTO.fromImage(newImage)
+    }
+
+    private fun assignMainImage(imageType: String, restaurant: Restaurant, newImage: Image) {
+        when (imageType) {
+            "main" -> {
+                restaurant.mainImage = newImage
+                restaurant.images.add(newImage)
+                restaurantService.save(restaurant)
+            }
+        }
+    }
+
+    private fun saveImage(image: MultipartFile, uuidName: String, restaurant: Restaurant): Image {
+        return imageService.save(Image(
+                fileName = image.originalFilename,
+                uuid = uuidName,
+                restaurant = restaurant
+        ))
+    }
+
+    private fun saveFullSizeThumbnail(uuidName: String, image: MultipartFile) {
         val thumbnail = File("/$TMP/${THUMBNAIL}_$uuidName.png")
         val fullSize = File("/$TMP/${FULL_SIZE}_$uuidName.png")
         imageService.resize(image, thumbnail, 300, 300)
         imageService.toFile(image, fullSize)
         Files.copy(thumbnail.inputStream(), thumbnailLocation.resolve("$uuidName.png"))
         Files.copy(fullSize.inputStream(), fullSizeLocation.resolve("$uuidName.png"))
+    }
 
-        return ImageDTO.fromImage(imageService.save(Image(
-                fileName = image.originalFilename,
-                uuid = uuidName,
-                restaurant = restaurant
-        )))
+    private fun validateImage(image: MultipartFile) {
+        if (image.isEmpty) throw ServiceException(HttpStatus.BAD_REQUEST, "Image is empty.")
+        if (!image.isImageType()) throw ServiceException(HttpStatus.BAD_REQUEST, "Wrong image type.")
     }
 }
