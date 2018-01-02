6c699c7062849c123277569319a6c3f60188c073
Denis Olek <denis.olek@gmail.com>
2018-01-02 16:27:47 +0100

    TAB-468 version update 0.24.0 &&  accepting "&" in create restaurant DTO

--- src/main/kotlin/pl/denisolek/panel/identity/DTO/CreateRestaurantDTO.kt
+++ src/main/kotlin/pl/denisolek/panel/identity/DTO/CreateRestaurantDTO.kt
@@ -8,7 +8,7 @@ import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 data class CreateRestaurantDTO(
-        @field:Pattern(regexp = "^[a-z A-Z0-9żźćńółęąśŻŹĆĄŚĘŁÓŃ]+(-[a-z A-Z0-9żźćńółęąśŻŹĆĄŚĘŁÓŃ]+)?\$", message = "Name accepts a-z A-Z 0-9 - & and spaces.")
+        @field:Pattern(regexp = "^[a-z A-Z0-9&żźćńółęąśŻŹĆĄŚĘŁÓŃ]+(-[a-z A-Z0-9&żźćńółęąśŻŹĆĄŚĘŁÓŃ]+)?", message = "Name accepts a-z A-Z 0-9 - & and spaces.")
         @field:NotBlank
         var name: String,
 
