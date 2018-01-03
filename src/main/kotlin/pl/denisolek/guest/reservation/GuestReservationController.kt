e7e65f2752bb9aa5eb54d1f94dfde668e442a205
Denis Olek <denis.olek@gmail.com>
2018-01-03 12:59:58 +0100

    TAB-532 added annotations in controller

--- src/main/kotlin/pl/denisolek/guest/reservation/GuestReservationController.kt
+++ src/main/kotlin/pl/denisolek/guest/reservation/GuestReservationController.kt
@@ -1,10 +1,12 @@
 package pl.denisolek.guest.reservation
 
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import pl.denisolek.guest.reservation.DTO.CreateReservationGuestDTO
+import javax.validation.Valid
 
 @RestController
 class GuestReservationController(val guestReservationService: GuestReservationService): GuestReservationApi {
-    override fun addReservation(createReservationGuestDTO: CreateReservationGuestDTO) =
+    override fun addReservation(@RequestBody @Valid createReservationGuestDTO: CreateReservationGuestDTO) =
             guestReservationService.addReservation(createReservationGuestDTO)
 }
